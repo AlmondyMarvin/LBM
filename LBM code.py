b5fb695fcc1fd1aab7b06b500cda548bde77e0da
AlmondyMarvin <ricardo.parra@ucalgary.ca>
2022-01-23 18:06:05 -0700

    Just added some general variables

--- LBM code.py
+++ LBM code.py
@@ -1,3 +1,15 @@
 import numpy as np
-a = np.array([1, 2, 3])
-print(a)
+from matplotlib import pyplot as plt
+
+
+Nx = 100    # dicretizaion of x , this is a rectangle
+Ny = 25    # dicretizaion of y
+rho0 = 100    # density
+tau = 0.6    # collision timescale
+Nt = 4000   # timesteps
+
+# based upon D2Q9 structure
+w = np.array([4 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 36, 1 / 36, 1 / 36, 1 / 36]) #weights of directions
+e = np.array([[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, -1],[1, -1], [-1, 1]]) #indices of each direction
+
+print(e)
