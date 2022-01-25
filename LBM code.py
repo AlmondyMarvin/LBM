01e92e74cef6d201c6f668a7abbe63baf96c2536
AlmondyMarvin <ricardo.parra@ucalgary.ca>
2022-01-24 22:28:41 -0700

    Added the streaming/propagation

--- LBM code.py
+++ LBM code.py
@@ -2,14 +2,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-Nx = 100    # dicretizaion of x , this is a rectangle
-Ny = 25    # dicretizaion of y
+nx, ny = 400, 100   # number of cells
 rho0 = 100    # density
 tau = 0.6    # collision timescale
 Nt = 4000   # timesteps
+L = 9
 
 # based upon D2Q9 structure
 w = np.array([4 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 36, 1 / 36, 1 / 36, 1 / 36]) #weights of directions
-e = np.array([[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, -1],[1, -1], [-1, 1]]) #indices of each direction
+e = np.array([[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, -1],[1, -1], [-1, 1]]) #lattice velocities
 
-print(e)
+grid = np.zeros((nx,ny,L)) # Create the main grid which contains the 9 variables of each cell
+
+fI = np.zeros((nx,ny,L))
+fO = np.zeros((nx,ny,L))
+
+def stream(L):
+    for i in range (L):
+        fI[i,:,:] = np.roll(np.roll(fO[i,:,:], e[i,0], axis=0), e[i,1] , axis = 1)
+
